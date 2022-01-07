7140972648c911dd1410d1058eb475643d527d60
XiaQiong <86909923+XiaQiong@users.noreply.github.com>
2022-01-07 22:52:10 +1300

    Delete LinearKernel.R

--- LinearKernel.R
+++
@@ -1,8 +0,0 @@
-#'  Calculate the Linear kernel for genomic matrix
-#'
-#' @param Gen Gen is a n*p genomic matrix with n subjects and p SNPs.
-
-#' @return The kernel matrix of the corresponding genomic matrix.
-#' @export
-
-Fun_Klinear=function(Gen) Gen%*%t(Gen)/ncol(Gen)
